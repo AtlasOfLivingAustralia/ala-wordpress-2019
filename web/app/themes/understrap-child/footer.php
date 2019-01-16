10e07b448e4aa8403265bfcf1decde56e46bf423
Jeff <jeff@jeffmiller.com.au>
2019-01-16 15:25:33 +1000

    Footer Top

--- web/app/themes/understrap-child/footer.php
+++ web/app/themes/understrap-child/footer.php
@@ -17,6 +17,9 @@ $custom_logo_attr = array(
     'class'    => 'custom-logo',
     'itemprop' => 'logo',
 );
+
+
+
 ?>
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
@@ -25,8 +28,8 @@ $custom_logo_attr = array(
 
 	<div class="footer-top">
         <div class="<?php echo esc_attr( $container ); ?>">
-            <div class="row">
-                <div class="col-md-4">
+            <div class="row align-items-center">
+                <div class="col-md-12 col-lg-4 align-center logo-column">
                     <?= wp_get_attachment_image( $custom_logo_id, 'full', false, $custom_logo_attr ); ?>
                     <div class="account">
                         <a href="#" class="btn btn-outline-white btn-sm">Sign Up</a>
@@ -34,11 +37,31 @@ $custom_logo_attr = array(
                         <?php get_template_part('template-parts/social-icons'); ?>
                     </div>
                 </div><!--col end -->
-                <div class="col-md-4">
-
+                <div class="col-md-6 col-lg-4 content-column">
+                    <?php   $imageCol1 = get_field('col_1_image', 'option');
+                            $contentCol1 = get_field('col_1_content', 'option');
+                    ?>
+                    <div class="d-flex">
+                        <div class="image">
+                            <img src="<?= $imageCol1['sizes']['medium']; ?>" alt="<?= $imageCol1['alt']; ?>" />
+                        </div>
+                        <div class="content">
+                            <?= $contentCol1; ?>
+                        </div>
+                    </div>
                 </div><!--col end -->
-                <div class="col-md-4">
-
+                <div class="col-md-6 col-lg-4 content-column">
+                    <?php   $imageCol2 = get_field('col_2_image', 'option');
+                            $contentCol2 = get_field('col_2_content', 'option');
+                    ?>
+                    <div class="d-flex">
+                        <div class="image">
+                            <img src="<?= $imageCol2['sizes']['medium']; ?>" alt="<?= $imageCol2['alt']; ?>" />
+                        </div>
+                        <div class="content">
+                            <?= $contentCol2; ?>
+                        </div>
+                    </div>
                 </div><!--col end -->
             </div><!-- row end -->
         </div><!-- container end -->
@@ -48,9 +71,7 @@ $custom_logo_attr = array(
         <div class="<?php echo esc_attr( $container ); ?>">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="site-info">
-                        <?php understrap_site_info(); ?>
-                    </div><!-- .site-info -->
+
                 </div><!--col end -->
             </div><!-- row end -->
         </div><!-- container end -->
