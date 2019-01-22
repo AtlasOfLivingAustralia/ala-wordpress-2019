5af2c275380b4b8a3894b358c51b2475e5f9faf8
Jeff <jeff@jeffmiller.com.au>
2019-01-22 14:58:46 +1000

    Build featured-image template part

--- web/app/themes/pvtl/archive.php
+++ web/app/themes/pvtl/archive.php
@@ -1,17 +1,13 @@
 <?php
 /**
- * The main template file.
+ * The template for displaying archive pages.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package understrap
  */
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
@@ -20,9 +16,11 @@ get_header();
 $container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php get_template_part('template-parts/featured-image'); ?>
+<?php if (is_front_page() && is_home()) : ?>
+	<?php get_template_part('global-templates/hero'); ?>
+<?php endif; ?>
 
-<div class="wrapper" id="index-wrapper">
+<div class="wrapper" id="archive-wrapper">
 
 	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
@@ -85,6 +83,6 @@ $container = get_theme_mod('understrap_container_type');
 
 	</div><!-- #content -->
 
-</div><!-- #index-wrapper -->
+</div><!-- #archive-wrapper -->
 
 <?php get_footer(); ?>
