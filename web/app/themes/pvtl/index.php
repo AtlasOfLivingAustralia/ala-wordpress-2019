df56cfa4d6a2c93bf581bec7f5dd1eacb3c0f059
Jeff <jeff@jeffmiller.com.au>
2019-01-22 13:05:20 +1000

    Post List

--- web/app/themes/pvtl/index.php
+++ web/app/themes/pvtl/index.php
@@ -11,35 +11,45 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php if ( is_front_page() && is_home() ) : ?>
-	<?php get_template_part( 'global-templates/hero' ); ?>
+<?php if (is_front_page() && is_home()) : ?>
+	<?php get_template_part('global-templates/hero'); ?>
 <?php endif; ?>
 
 <div class="wrapper" id="index-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
-		<div class="row">
 
-			<!-- Do the left sidebar check and opens the primary div -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+		<main class="site-main post-list" id="main">
+			<div class="row">
+				<?php if (have_posts()) : ?>
 
-			<main class="site-main post-list" id="main">
+					<?php /* Start the Loop */
+					$counter = 0;
+					?>
 
-				<?php if ( have_posts() ) : ?>
+					<?php while (have_posts()) : the_post();
+						// Wrap first post in a larger 8 col div
+						if ($counter == 0) {
+							echo '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">';
+							//Wrap second post in opening 4 col div
+						} else if ($counter == 1) {
+							echo '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">';
+							//Wrap all posts from 4th onwards in the standard 4 col div
+						} else if ($counter > 2) {
+							echo '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">';
+						}
 
-					<?php /* Start the Loop */ ?>
-
-					<?php while ( have_posts() ) : the_post(); ?>
+						?>
 
 						<?php
 
@@ -48,26 +58,31 @@ $container = get_theme_mod( 'understrap_container_type' );
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
+						get_template_part('loop-templates/content', get_post_format());
 						?>
 
-					<?php endwhile; ?>
+						<?php
+						//Close first large 8 col div
+						if ($counter == 0) {
+							echo '</div>';
+							// close all cols from 3rd post onwards
+						} else if ($counter >= 2) {
+							echo '</div>';
+						}
+						$counter++;
+					endwhile; ?>
 
 				<?php else : ?>
 
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+					<?php get_template_part('loop-templates/content', 'none'); ?>
 
 				<?php endif; ?>
+			</div><!-- .row -->
+		</main><!-- #main -->
 
-			</main><!-- #main -->
+		<!-- The pagination component -->
+		<?php understrap_pagination(); ?>
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
-
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-		</div><!-- .row -->
 
 	</div><!-- #content -->
 
