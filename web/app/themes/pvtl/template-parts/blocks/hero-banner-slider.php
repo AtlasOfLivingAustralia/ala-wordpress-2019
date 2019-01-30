ddf0bdddd35e2a45eab84f4cc3da3f0d73ff4d88
Jeff <jeff@jeffmiller.com.au>
2019-01-30 12:02:01 +1000

    Fix search button alignment/sizing

--- web/app/themes/pvtl/template-parts/blocks/hero-banner-slider.php
+++ web/app/themes/pvtl/template-parts/blocks/hero-banner-slider.php
@@ -76,7 +76,7 @@ class HeroBannerSlider extends RegisterBlocks
                                     <label for="search" class="sr-only">Search Species Records</label>
                                     <input type="search" class="form-control flex-grow-1" id="search" placeholder="Search 116,724 Species Records...">
                                 </div>
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary-dark">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
                                         <defs>
                                             <style>
