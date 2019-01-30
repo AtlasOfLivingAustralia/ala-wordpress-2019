b1274c389b392e8374d61db390de53eb5975adaa
Jeff <jeff@jeffmiller.com.au>
2019-01-30 11:56:38 +1000

    Change all headings from Black 900 to Bold 700

--- web/app/themes/pvtl/functions.php
+++ web/app/themes/pvtl/functions.php
@@ -29,7 +29,7 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:900|Roboto:400,400i,500' );
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:700|Roboto:400,400i,500' );
     wp_enqueue_script( 'jquery');
 	wp_enqueue_script( 'popper-scripts', get_stylesheet_directory_uri() . '/js/popper.min.js', array(), false);
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
