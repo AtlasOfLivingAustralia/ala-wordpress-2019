d70e6264efdfec3bc3fe34ea67e895059a0c2959
Matt Andrews <Matt.Andrews@csiro.au>
2019-06-18 11:44:40 +1000

    Never show comments.

--- web/app/themes/pvtl/page-templates/right-sidebaranchors.php
+++ web/app/themes/pvtl/page-templates/right-sidebaranchors.php
@@ -41,9 +41,9 @@ if (!is_front_page()) {
 
 						<?php
 						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
+						// if ( comments_open() || get_comments_number() ) :
+						// 	comments_template();
+						// endif;
 						?>
 
 					<?php endwhile; // end of the loop. ?>
