b9623d2f640a11d31de1424dc76138d4a600fcb0
Matt Andrews <Matt.Andrews@csiro.au>
2019-07-16 17:17:27 +1000

    don’t show thumbnail/featured image on single-blog-post pages.

--- web/app/themes/pvtl/loop-templates/content-single.php
+++ web/app/themes/pvtl/loop-templates/content-single.php
@@ -30,7 +30,8 @@ $postLink = get_post_type_archive_link($postType);
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	<?php /* echo get_the_post_thumbnail( $post->ID, 'large' ); */
+	?>
 
 	<div class="entry-content">
 
