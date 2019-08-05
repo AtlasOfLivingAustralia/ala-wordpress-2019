e78fe8396ff0c8fcfaa8261b35ba45e6bcac45af
Matt Andrews <Matt.Andrews@csiro.au>
2019-08-05 12:06:30 +1000

    add id meta

--- web/app/themes/pvtl/header.php
+++ web/app/themes/pvtl/header.php
@@ -24,6 +24,7 @@ if ( get_field( 'auth_buttons_display', 'option' ) ) {
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="id" content="<?php the_ID(); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
