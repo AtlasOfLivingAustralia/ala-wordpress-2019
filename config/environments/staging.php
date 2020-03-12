b7c6fd8b2826cde2ccc63f8f2befd9ac1b547f19
Matt Andrews <Matt.Andrews@csiro.au>
2020-03-13 10:34:59 +1100

    turn on debug for staging (wptest)

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -5,6 +5,10 @@
 
 use Roots\WPConfig\Config;
 
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('SCRIPT_DEBUG', true);
+
 /**
  * You should try to keep staging as close to production as possible. However,
  * should you need to, you can always override production configuration values
