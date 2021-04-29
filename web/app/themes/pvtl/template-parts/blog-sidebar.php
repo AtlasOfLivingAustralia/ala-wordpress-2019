38eb0845a657ce63fbda7155aa5c41e6a8ade16c
Matt Andrews <Matt.Andrews@csiro.au>
2021-04-29 19:09:46 +1000

    blog sidebar with Newsletters and Impact stories

---
+++ web/app/themes/pvtl/template-parts/blog-sidebar.php
@@ -0,0 +1,46 @@
+<!-- blog sidebar -->
+<article>
+	<h3>ALA Newsletters</h3>
+	<ul>
+<?php
+	$sideargs  = array(
+	    'posts_per_page'  => 10,
+	    'offset'          => 0,
+	    'category_name'   => 'newsletter',
+	    'tag'             => 'newsletter',
+	    'orderby'         => 'post_date',
+	    'order'           => 'ASC',
+	    'post_type'       => 'post',
+	    'post_status'     => 'publish',
+	    'suppress_filters' => true ); 
+	$sideposts = get_posts($sideargs);
+    foreach ($sideposts as $post) :
+    ?>
+		<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+<?php endforeach; ?>
+	</ul>
+</article>
+<article>
+	<h3>ALA Impact stories</h3>
+	<ul>
+<?php
+	$sideargs  = array(
+	    'posts_per_page'  => 10,
+	    'offset'          => 0,
+	    'category_name'   => 'impact-stories',
+	    'orderby'         => 'post_date',
+	    'order'           => 'ASC',
+	    'post_type'       => 'post',
+	    'post_status'     => 'publish',
+	    'suppress_filters' => true ); 
+	$sideposts = get_posts($sideargs);
+    foreach ($sideposts as $post) :
+    ?>
+		<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+<?php 
+	endforeach; 
+	//wp_reset_query();
+?>
+	</ul>
+</article>
+<!-- /blog sidebar -->
