c6434bf8ae27cac7ccc39ab9895e8721c79fb896
Matt Andrews <Matt.Andrews@csiro.au>
2022-07-19 12:35:59 +1000

    revert to ACF Pro 5.11.4 (5.12 is still broken for home page edit)

--- web/app/plugins/advanced-custom-fields-pro/includes/api/api-helpers.php
+++ web/app/plugins/advanced-custom-fields-pro/includes/api/api-helpers.php
@@ -2691,15 +2691,20 @@ function acf_isset_termmeta( $taxonomy = '' ) {
 
 }
 
-/**
- * This function will walk through the $_FILES data and upload each found.
- *
- * @date    25/10/2014
- * @since   5.0.9
- *
- * @param array $ancestors An internal parameter, not required.
- * @return void
- */
+
+/*
+*  acf_upload_files
+*
+*  This function will walk througfh the $_FILES data and upload each found
+*
+*  @type    function
+*  @date    25/10/2014
+*  @since   5.0.9
+*
+*  @param   $ancestors (array) an internal parameter, not required
+*  @return  n/a
+*/
+
 function acf_upload_files( $ancestors = array() ) {
 
 	// vars
@@ -2746,21 +2751,15 @@ function acf_upload_files( $ancestors = array() ) {
 
 	}
 
-	// Bail early if file has error (no file uploaded).
+	// bail ealry if file has error (no file uploaded)
 	if ( $file['error'] ) {
+
 		return;
+
 	}
 
-	$field_key  = end( $ancestors );
-	$nonce_name = $field_key . '_file_nonce';
-	$file_nonce = isset( $_REQUEST['acf'][ $nonce_name ] ) ? $_REQUEST['acf'][ $nonce_name ] : false;
-
-	if ( ! $file_nonce || ! wp_verify_nonce( $file_nonce, 'acf/file_uploader_nonce/' . $field_key ) ) {
-		return;
-	}
-
-	// Assign global _acfuploader for media validation.
-	$_POST['_acfuploader'] = $field_key;
+	// assign global _acfuploader for media validation
+	$_POST['_acfuploader'] = end( $ancestors );
 
 	// file found!
 	$attachment_id = acf_upload_file( $file );
@@ -2770,6 +2769,7 @@ function acf_upload_files( $ancestors = array() ) {
 	acf_update_nested_array( $_POST, $ancestors, $attachment_id );
 
 }
+
 
 /*
 *  acf_upload_file
@@ -3071,29 +3071,39 @@ function acf_get_attachment( $attachment ) {
 }
 
 
-/**
- *  This function will truncate and return a string
- *
- *  @date    8/08/2014
- *  @since   5.0.0
- *
- *  @param string $text   The text to truncate.
- *  @param int    $length The number of characters to allow in the string.
- *
- *  @return  string
- */
+/*
+*  acf_get_truncated
+*
+*  This function will truncate and return a string
+*
+*  @type    function
+*  @date    8/08/2014
+*  @since   5.0.0
+*
+*  @param   $text (string)
+*  @param   $length (int)
+*  @return  (string)
+*/
+
 function acf_get_truncated( $text, $length = 64 ) {
+
+	// vars
 	$text       = trim( $text );
-	$the_length = function_exists( 'mb_strlen' ) ? mb_strlen( $text ) : strlen( $text );
+	$the_length = strlen( $text );
 
-	$cut_length = $length - 3;
-	$return     = function_exists( 'mb_substr' ) ? mb_substr( $text, 0, $cut_length ) : substr( $text, 0, $cut_length );
+	// cut
+	$return = substr( $text, 0, ( $length - 3 ) );
 
-	if ( $the_length > $cut_length ) {
+	// ...
+	if ( $the_length > ( $length - 3 ) ) {
+
 		$return .= '...';
+
 	}
 
+	// return
 	return $return;
+
 }
 
 /*
